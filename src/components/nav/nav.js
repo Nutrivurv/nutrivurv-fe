6cc9b0c527091c7a8f26058156c2712ee9f549d3
taty2010 <shydalys123@gmail.com>
2020-05-27 21:38:04 -0500

    finished nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -6,9 +6,17 @@ const Nav = () => {
     <div className="navbar navbar-light bg-light flex-column flex-md-row">
         <a className="navbar-brand" href="#">Nutrivurv</a>
       <nav className="col-md-4 col-lg-3 col-xl-2 align-self-center">
-        <ul className="nav justify-content-center">
-          <li className='col-md-6 nav-item'><a className="nav-link px-0" href='#'>Sign In</a></li>
-          <li className='col-md-6 nav-item'><a className="nav-link px-0" href='#'>Sign Up</a></li>
+        <ul className="nav flex-md-row">
+          <li className='nav-item col-md-6 align-self-center'>
+            <a className="nav-link d-flex justify-content-center px-0" href='#'>
+              Sign In
+            </a>
+          </li>
+          <li className='nav-item col-md-6 align-self-center'>
+            <a className="nav-link d-flex justify-content-center px-0" href='#'>
+              Sign Up
+            </a>
+          </li>
         </ul>
       </nav>
     </div>
