7dee43fd778cce35ffd06d784165b63837e6e23c
taty2010 <shydalys123@gmail.com>
2020-05-29 00:19:38 -0500

    Changed route path for Landing page to /

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import "./App.scss";
 import "bootswatch/dist/flatly/bootstrap.min.css";
 import Nav from "./components/nav/nav";
 
-
 function App() {
   
   return (
     <div className="App container-fluid">
       <Nav></Nav>
-      <Route path="/landing" component={LandingPage} />
+      <Route path="/" component={LandingPage} />
     </div>
   );
 }
