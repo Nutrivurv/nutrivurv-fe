cdac58a72aba104fa3471901250761a0bebc9555
alesslongaretti <alesslongaretti@gmail.com>
2020-06-01 19:50:27 -0400

    validation and styling completed

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -4,12 +4,12 @@ import { ReactComponent as SignInImage } from "../../assets/GirlComptr.svg";
 
 const SignIn = () => {
   return (
-    <div className="row d-flex justify-content-around">
-      <div>
-        <h2 className= "text-center"> Sign In </h2>
-        <p>Let's start crushing those goals!</p>
+    <div className="row d-flex justify-content-around ml-5 pl-4">
+      <div className="col-md-4 pt-5">
+        <h2 className="text-center"> Sign In </h2>
+        <p className="text-center"> Let's start crushing those goals!</p>
         <form role="form">
-          <div className="form-group ">
+          <div className="form-group">
             <label htmlFor="inputEmail" className="control-label">
               E-mail
             </label>
@@ -23,7 +23,7 @@ const SignIn = () => {
             <div className="help-block with-errors"></div>
           </div>
 
-          <div className="form-group col-sm-15">
+          <div className="form-group">
             <label htmlFor="input-password">Password</label>
             <input
               type="password"
@@ -35,14 +35,22 @@ const SignIn = () => {
             />
           </div>
 
-          <button type="submit" className="btn-primary form-group col-sm-12">
+          <button
+            type="submit"
+            className="btn-primary form-group col-md-12 rounded-sm"
+          >
             Sign In
           </button>
         </form>
-        <p>Not a member? <Link to="/signup">Sign Up</Link></p>
+        <p className="text-center">
+          Not a member? <Link to="/signup">Sign Up</Link>
+        </p>
       </div>
       <div>
-        <SignInImage className="col-md-15" />
+        <SignInImage
+          className="img-responsive pb-5 ml-3 pl-5"
+          alt="Responsive SignIn image"
+        />
       </div>
     </div>
   );
