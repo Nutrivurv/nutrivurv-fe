ff5aa07095da0c18372dfedefd4cda8a0acda0c9
Dave Vazquez <dmvaz.li@gmail.com>
2020-05-31 21:48:45 -0400

    small change to test heroku buildpack

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootswatch/dist/flatly/bootstrap.min.css";
+import "./App.scss";
+
 import React from "react";
 import { Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage.js";
 import SignUp from "./components/SignUp";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootswatch/dist/flatly/bootstrap.min.css";
-import "./App.scss";
 import Nav from "./components/nav/nav";
 
 function App() {
-  
   return (
     <div className="App container-fluid">
       <Nav></Nav>
