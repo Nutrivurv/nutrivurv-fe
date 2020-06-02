650eb0731b5d5875023dd8c93de3772391f018ca
Dave Vazquez <dmvaz.li@gmail.com>
2020-06-01 22:52:03 -0400

    sets up CI for publishing test coverage for CC

---
+++ cypress/support/index.js
@@ -0,0 +1,9 @@
+import "@cypress/code-coverage/support";
+
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+  require("@cypress/code-coverage/task")(on, config);
+
+  return config;
+};
