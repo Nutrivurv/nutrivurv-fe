19791b674a9ba93a9ca8fd3222b0f57908153600
alesslongaretti <alesslongaretti@gmail.com>
2020-06-01 22:41:25 -0400

    deleted duplicate signin file and fixed app.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,12 @@ import { Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage.js";
 import SignIn from "./components/SignIn/SignIn";
 import SignUp from "./components/SignUp";
-
-import "./App.scss";
-import "bootswatch/dist/flatly/bootstrap.min.css";
-import "./App.scss";
-
 import Nav from "./components/nav/nav";
 
 function App() {
   return (
     <div className="App container-fluid p-2">
       <Nav></Nav>
-      <Route path="/signin" component={SignIn} />
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/signin" component={SignIn} />
       <Route exact path="/signup" component={SignUp} />
