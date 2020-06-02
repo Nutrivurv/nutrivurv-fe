64afc503b5003703c2de6700f9208be65bc07bba
taty2010 <shydalys123@gmail.com>
2020-06-02 13:38:36 -0500

    added logo to nav and made styling changes to landing and marketing component

--- src/components/Marketing/marketing.js
+++ src/components/Marketing/marketing.js
@@ -1,32 +1,33 @@
 import React from "react";
-import { ReactComponent as LandingImage }from "../../assets/Splash.svg"
-import { Link } from "react-router-dom"; 
+import { ReactComponent as LandingImage } from "../../assets/Splash.svg";
+import { Link } from "react-router-dom";
 
 const Marketing = () => {
-
-  return(
-    <div className="marketing" style={{height: "110vh"}}>
-      <section className=" ml-lg-5">
-        <div className="ml-5 ">
-          <h2 className="row-8 ml-5">
-            Tracking your calories <br/>
+  return (
+    <div className="marketing">
+      <section className=" ml-md-5">
+        <div className="ml-5 mt-3">
+          <h1 className="row-8 ml-5" style={{ fontSize: "5.5vw" }}>
+            Tracking your calories <br />
             just got easier.
-          </h2>
-          <p className="row-8 ml-5">
-            Nutrivurv is an easy to use <br/>
-            motivating food and nutrition <br/>
+          </h1>
+          <p className="row-8 ml-5" style={{ fontSize: "2.5vw" }}>
+            Nutrivurv is an easy to use <br />
+            motivating food and nutrition <br />
             tracking app.
           </p>
-          <Link type="button" to="/signup" >
-            <button className="btn btn-primary ml-5 btn-primary">Sign Up</button>
-            </Link>
+          <Link type="button" to="/signup">
+            <button className="btn btn-primary ml-5 btn-primary">
+              Sign Up
+            </button>
+          </Link>
         </div>
-        <figure className="row px-0 ml-5">
-            <LandingImage width="300px"/>
+        <figure className="row px-0 mt-5 ml-5">
+          <LandingImage className="lp-img" />
         </figure>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Marketing
+export default Marketing;
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,11 +1,14 @@
 
 import React from 'react';
 import { Route, Link } from "react-router-dom";
+import Logo from "../../assets/nvlogo.png";
 
 const Nav = () => {
   return (
     <div className="navbar navbar-light bg-light flex-column flex-md-row">
-        <Link to="/" className="navbar-brand" href="#">Nutrivurv</Link>
+      <Link to="/" className="navbar-brand ml-3" href="#">
+        <img src={Logo} alt="Nutrivurv logo"></img>
+      </Link>
       <nav className="col-md-4 col-lg-3 col-xl-2 align-self-center">
         <ul className="nav flex-md-row">
           <li className="nav-item col-md-6 align-self-center">
