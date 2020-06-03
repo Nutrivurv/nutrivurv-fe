12e9c38f7bc8014244efa8ed6ded6f822c4b3be2
taty2010 <shydalys123@gmail.com>
2020-06-02 21:24:12 -0500

    added dietary preference radio form

---
+++ src/components/on-boarding/dietaryPref.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as SignInImage } from "../../assets/GirlComptr.svg";
+
+const DietaryPref = () => {
+  const customRadio = "custom-control custom-radio mb-2";
+  return (
+    <div className="row d-flex justify-content-around ml-5 pl-4">
+      <div className="col-md-5 pt-5">
+        <h2 className="text-center mb-5"> Dietary Preference </h2>
+        <form className="form-group" role="form col-md-5">
+          <div className={customRadio}>
+            <input
+              type="radio"
+              id="customRadio1"
+              name="customRadio"
+              className="custom-control-input"
+              disabled=""
+            />
+            <label className="custom-control-label" htmlFor="customRadio1">
+              Keto
+            </label>
+          </div>
+          <div className={customRadio}>
+            <input
+              type="radio"
+              id="customRadio2"
+              name="customRadio"
+              className="custom-control-input"
+            />
+            <label className="custom-control-label" htmlFor="customRadio2">
+              Paleo
+            </label>
+          </div>
+          <div className={customRadio}>
+            <input
+              type="radio"
+              id="customRadio3"
+              name="customRadio"
+              className="custom-control-input"
+              disabled=""
+            />
+            <label className="custom-control-label" htmlFor="customRadio3">
+              US. Gov. Nutrition Guidlines
+            </label>
+          </div>
+          <div className={customRadio}>
+            <input
+              type="radio"
+              id="customRadio4"
+              name="customRadio"
+              className="custom-control-input"
+              disabled=""
+            />
+            <label className="custom-control-label" htmlFor="customRadio4">
+              None
+            </label>
+          </div>
+
+          <button
+            type="submit"
+            className="btn-primary form-group col-md-6 rounded-sm mt-5"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      <div>
+        <SignInImage
+          className="img-responsive pb-5 ml-3 pl-5"
+          alt="Responsive SignIn image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DietaryPref;
