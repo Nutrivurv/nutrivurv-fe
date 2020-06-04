944ecf4bea710b613b33c0c632e4560f0cf878f4
alesslongaretti <alesslongaretti@gmail.com>
2020-06-04 00:11:36 -0400

    completed styling

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -12,10 +12,10 @@ const SignUp = ({ setStep }) => {
     setStep("BMI");
   };
   return (
-    <div className="d-block">
-      <div className="d-flex justify-content-center">
-        <div className="d-block p-5 m-5">
-          <h1 className="text-center pb-3"> Sign Up </h1>
+    <div>
+      <div className="d-flex justify-content-around mt-5">
+        <div className="mt-5 ml-6 col-md-3">
+          <h1 className="text-center pb-3 font-weight-bolder">Sign Up</h1>
           <h4 className="text-center pb-5">
             You&apos;re one step closer to your goals!
           </h4>
@@ -24,7 +24,7 @@ const SignUp = ({ setStep }) => {
               <label className="mb-0">Your Name</label>
               <input
                 name="username"
-                className="rounded p-2 w-100 border border-primary"
+                className="rounded p-3 w-100 border border-primary"
                 placeholder="First and Last Name"
                 ref={register({
                   required: "Required",
@@ -41,7 +41,7 @@ const SignUp = ({ setStep }) => {
               <label className="mb-0">E-mail</label>
               <input
                 name="email"
-                className="rounded p-2 w-100 border border-primary"
+                className="rounded p-3 w-100 border border-primary"
                 placeholder="email@email.com"
                 ref={register({
                   required: "Required",
@@ -58,7 +58,7 @@ const SignUp = ({ setStep }) => {
               <label className="mb-0">Password</label>
               <input
                 name="password"
-                className="rounded p-2 w-100 border border-primary"
+                className="rounded p-3 w-100 border border-primary"
                 placeholder="8-12 characters"
                 type="password"
                 ref={register({
@@ -81,7 +81,7 @@ const SignUp = ({ setStep }) => {
               <input
                 name="password_repeat"
                 placeholder="Confirm Password"
-                className="rounded p-2 w-100 border border-primary"
+                className="rounded p-3 w-100 border border-primary"
                 type="password"
                 ref={register({
                   validate: (value) =>
@@ -102,14 +102,15 @@ const SignUp = ({ setStep }) => {
             </button>
           </form>
           <div className="d-flex justify-content-center mt-3 p-2">
-            <div className="d-flex justify-content-center">
-              <p className="mr-2"> Already a member? </p>
-              <Link to="/signin"> Sign In </Link>
-            </div>
+            <p className="mr-2"> Already a member? </p>
+            <Link to="/signin"> Sign In </Link>
           </div>
         </div>
-        <div className="pt-2 mt-4 d-none d-xs-none d-sm-none d-md-block">
-          <GirlComputerImg className="img-responsive" />
+        <div>
+          <GirlComputerImg
+            className=" col img-responsive mt-5 mr-6 pt-5 pr-5"
+            alt="Responsive Sign Up image"
+          />
         </div>
       </div>
     </div>
