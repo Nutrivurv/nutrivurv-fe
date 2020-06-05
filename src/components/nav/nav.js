d9974f2a5fb5efa7e596713fd58ad54fdd9e56c1
alesslongaretti <alesslongaretti@gmail.com>
2020-06-05 17:10:50 -0400

    fixed bugs by deleting the a tags inside of NavLink

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -11,25 +11,19 @@ const Nav = () => {
       <nav className="col-md-4 col-lg-3 col-xl-2 align-self-center">
         <ul className="nav flex-md-row">
           <li className="nav-item col-md-6 align-self-center">
-            <NavLink to="/signin">
-              <a
-                id="signIn-link"
-                className="nav-link d-flex justify-content-center px-0"
-                href="#"
-              >
-                Sign In
-              </a>
+            <NavLink
+              to="/signin"
+              className="nav-link d-flex justify-content-center px-0"
+            >
+              Sign In
             </NavLink>
           </li>
           <li className="nav-item col-md-6 align-self-center">
-            <NavLink to="/signup">
-              <a
-                id="signUp-link"
-                className="nav-link d-flex justify-content-center px-0"
-                href="#"
-              >
-                Sign Up
-              </a>
+            <NavLink
+              to="/signup"
+              className="nav-link d-flex justify-content-center px-0"
+            >
+              Sign Up
             </NavLink>
           </li>
         </ul>
