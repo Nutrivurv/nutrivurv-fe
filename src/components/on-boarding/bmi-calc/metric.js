341886bf117376f733115b18b28f97d68011fe4c
taty2010 <shydalys123@gmail.com>
2020-06-07 00:29:22 -0500

    removed calc function so BMI total will show automatically and added validation

--- src/components/on-boarding/bmi-calc/metric.js
+++ src/components/on-boarding/bmi-calc/metric.js
@@ -1,53 +1,65 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
-const Metric = ({ bmi, setBmi, setUser, user }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    calc(bmi);
-  };
-
+const Metric = ({ bmi, setBmi, setUser, user, setStep }) => {
   const handleChanges = (e) => {
     setBmi({ ...bmi, [e.target.name]: e.target.value });
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
-  const calc = (bmi) => {
-    let meters = Number(bmi.cm) / 100;
-    let height = meters * meters;
-    console.log(height);
-    console.log(meters);
-    const userBmi = Math.ceil(Number(bmi.kg) / height);
-    return setBmi({ ...bmi, total: userBmi });
+  const { register, handleSubmit, errors } = useForm({});
+  const onSubmit = async (data) => {
+    alert(JSON.stringify(data));
+    setStep("Complete");
   };
+
+  let meters = Number(bmi.cm) / 100;
+  let height = meters * meters;
+  bmi.total = Math.ceil(Number(bmi.kg) / height);
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={(e) => e.preventDefault()}>
         <div className="row my-3">
           <input
             className="form-control form-control-sm col-md-2 my-2 ml-3 mr-1"
-            type="text"
+            type="number"
             label="Your Height"
             name="cm"
             placeholder="cm"
             value={bmi.cm}
             onChange={handleChanges}
-          ></input>
+            ref={register({ required: true, maxLength: 3 })}
+          />
           <input
             className="form-control form-control-sm col-md-2 my-2 mx-1"
-            type="text"
+            type="number"
             label="Your Height"
             name="kg"
             placeholder="kg"
             value={bmi.kg}
             onChange={handleChanges}
-          ></input>
+            ref={register({ required: true, maxLength: 3 })}
+          />
         </div>
-        <button className="btn btn-outline-info mb-3" type="submit">
-          Calculate
+        <button
+          onClick={handleSubmit(onSubmit)}
+          className="btn btn-outline-info mb-3"
+          type="submit"
+        >
+          Submit
         </button>
+        {errors.cm && (
+          <small className="text-danger">{"Please add height"}</small>
+        )}
+        {errors.kg && (
+          <small className="text-danger">{"Please add weight"}</small>
+        )}
         <h5>
-          Your BMI: <span className="text-info">{bmi.total}</span>
+          Your BMI:{" "}
+          <span className="text-info">
+            {isNaN(bmi.total) ? "0" : bmi.total}
+          </span>
         </h5>
       </form>
     </>
--- src/components/on-boarding/bmi-calc/standard.js
+++ src/components/on-boarding/bmi-calc/standard.js
@@ -1,61 +1,79 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-const Standard = ({ setBmi, bmi, setUser, user }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    calc(bmi);
-  };
-
+const Standard = ({ setBmi, bmi, setUser, user, setStep }) => {
   const handleChanges = (e) => {
     setBmi({ ...bmi, [e.target.name]: e.target.value });
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
-  const calc = (bmi) => {
-    let height = Number(bmi.ft) * 12 + Number(bmi.inch);
-    const userBmi = Math.ceil(
-      (((703 * Number(bmi.weight)) / height) * height) / (height * height)
-    );
-    return setBmi({ ...bmi, total: userBmi });
+  const { register, handleSubmit, errors } = useForm({});
+  const onSubmit = async (data) => {
+    alert(JSON.stringify(data));
+    setStep("Complete");
   };
+
+  let height = Number(bmi.ft) * 12 + Number(bmi.inch);
+  bmi.total = Math.ceil(
+    (((703 * Number(bmi.weight)) / height) * height) / (height * height)
+  );
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={(e) => e.preventDefault()}>
         <div className="row my-3">
           <input
             className="form-control form-control-sm col-md-2 my-2 ml-3 mr-1"
-            type="text"
+            type="number"
             id="Height"
             name="ft"
             placeholder="ft"
             value={bmi.ft}
             onChange={handleChanges}
-          ></input>
+            ref={register({
+              required: true,
+              maxLength: 1,
+            })}
+          />
           <input
             className="form-control form-control-sm col-md-2 my-2 mx-1"
-            type="text"
+            type="number"
             label="In"
             name="inch"
             placeholder="inch"
             value={bmi.inch}
             onChange={handleChanges}
+            ref={register({ required: true, maxLength: 2 })}
           ></input>
           <input
             className="form-control form-control-sm col-md-2 my-2 mx-1"
-            type="text"
+            type="number"
             label="Your Height"
             name="weight"
             placeholder="lbs"
             value={bmi.weight}
             onChange={handleChanges}
+            ref={register({ required: true, maxLength: 3 })}
           ></input>
         </div>
-        <button className="btn btn-outline-info mb-3" type="submit">
-          Calculate
+        <button
+          onClick={handleSubmit(onSubmit)}
+          className="btn btn-outline-info mb-3"
+          type="submit"
+        >
+          Submit
         </button>
+        {errors.ft && (
+          <small className="text-danger">{"Please add height"}</small>
+        )}
+        {errors.weight && (
+          <small className="text-danger">{"Please add weight"}</small>
+        )}
         <h5>
-          Your BMI: <span className="text-info">{bmi.total}</span>
+          Your BMI:{" "}
+          <span className="text-info">
+            {isNaN(bmi.total) ? "0" : bmi.total}
+          </span>
         </h5>
       </form>
     </>
