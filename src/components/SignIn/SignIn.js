5d63e6403c63288ca4f3e67cc44c1c1079804099
alesslongaretti <alesslongaretti@gmail.com>
2020-06-12 01:03:47 -0400

    added react-hook-form to sign in component

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import { ReactComponent as SignInImage } from "../../assets/GirlComptr.svg";
 
 const SignIn = () => {
+  const { register, errors, handleSubmit } = useForm({});
+
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="d-flex justify mt-4 mx-5" style={{ height: "950px" }}>
       <div className="mt-5 col-xl-3 pt-5">
@@ -10,7 +17,7 @@ const SignIn = () => {
         <h4 className="text-center pb-5">
           Let&apos;s start crushing those goals!
         </h4>
-        <form>
+        <form data-toggle="validator" onSubmit={handleSubmit(onSubmit)}>
           <div className="form-group">
             <label className="mb-0">E-mail</label>
             <input
@@ -18,19 +25,42 @@ const SignIn = () => {
               className="rounded p-3 w-100 border border-primary"
               id="inputEmail"
               placeholder="email@email.com"
-              required
+              ref={register({
+                required: "Required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: "invalid email address",
+                },
+              })}
             />
+            {errors.email && (
+              <small className="text-danger">{errors.email.message}</small>
+            )}
           </div>
           <div className="form-group">
             <label className="mb-0">Password</label>
             <input
               name="password"
               data-minlength="8"
+              type="password"
               className="rounded p-3 w-100 border border-primary"
               id="inputPassword"
               placeholder="8-12 Characters"
-              required
+              ref={register({
+                required: "You must specify a password",
+                minLength: {
+                  value: 8,
+                  message: "Password must have at least 8 characters",
+                },
+                maxLength: {
+                  value: 12,
+                  message: "Password cannot have more than 12 characters",
+                },
+              })}
             />
+            {errors.password && (
+              <small className="text-danger">{errors.password.message}</small>
+            )}
           </div>
           <button
             type="submit"
