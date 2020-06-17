b80b1906cf42fad2d1656e2765ba001f8ce0b112
alesslongaretti <alesslongaretti@gmail.com>
2020-06-17 00:10:01 -0400

    add test for continue button - Passed

--- cypress/integration/onBoarding_spec.js
+++ cypress/integration/onBoarding_spec.js
@@ -27,11 +27,11 @@ describe("On-Boarding forms", () => {
   it("Should complete the Getting Personal form", () => {
     cy.get("form").within(() => {
       cy.get('input[name="age"]').type("26");
-      cy.get('select')
-        .select("female")
-        cy.get('input[name="goalWeight"]')
+      cy.get("select").select("female");
+      cy.get('input[name="goalWeight"]')
         .type("130")
         .should("have.value", "130");
+      cy.get("[data-cy=submit]").click();
     });
-  })
+  });
 });
