7b7c637a5e3f56ed3040fb79c989a56ae7711af3
alesslongaretti <alesslongaretti@gmail.com>
2020-06-18 22:00:35 -0400

    added the logo

---
+++ src/components/Dashboard/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as Logo } from "../../assets/Logo.svg";
+
+const Navigation = () => {
+  return (
+    <div>
+      <Logo className="logo" />
+    </div>
+  );
+};
+
+export default Navigation;
