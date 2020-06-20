07e1e7cf20f82eea1dbd1e60a804e2f21ca78798
alesslongaretti <alesslongaretti@gmail.com>
2020-06-19 22:43:10 -0400

    added style

--- src/components/Dashboard/Navigation.js
+++ src/components/Dashboard/Navigation.js
@@ -7,41 +7,64 @@ const Navigation = () => {
   return (
     <div className="wrapper">
       <nav id="sidebar">
-        <div>
+        <div className="d-flex justify-content-center my-3">
           <NavLink to="/" className="navbar-brand">
             <Logo className="logo" />
           </NavLink>
         </div>
         <div>
-          <div>
+          <div className="d-flex flex-column align-items-center my-5">
             <Placeholder />
-          </div>
-          <div>
-            <button>Add to Journal</button>
+            <h6 className="font-weight-bolder">Daily Intake</h6>
+            <div className="d-flex my-3">
+              <div>
+                <h3 className="mr-5">
+                  195
+                  <span className="pl-1" style={{ fontSize: "14px" }}>
+                    lbs
+                  </span>
+                </h3>
+                <h5 className="pl-2 font-weight-bolder">Weight</h5>
+              </div>
+              <div>
+                <h3>
+                  12
+                  <span className="pl-1" style={{ fontSize: "14px" }}>
+                    days
+                  </span>
+                </h3>
+                <h5 className="pl-2 font-weight-bolder">Streak</h5>
+              </div>
+            </div>
+            <div className="mt-5 mb-3">
+              <button
+                className="btn-primary rounded p-2 border border-primary"
+                style={{ width: "250px" }}
+              >
+                Add to Journal
+              </button>
+            </div>
           </div>
         </div>
-        <ul className="navbar-nav">
-          <li className="">
-            <NavLink to="/" className="nav-link d-flex justify-content-center">
-              <h5>Food</h5>
+        <ul className="navbar-nav pl-4">
+          <li className="side-link mb-3">
+            <NavLink to="/" className="nav-link">
+              <h4 className="font-weight-bold side-link-color">Food</h4>
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink
-              to="/"
-              className="nav-link d-flex justify-content-center  "
-            >
-              <h5>Progress</h5>
+          <li className="side-link mb-3">
+            <NavLink to="/" className="nav-link">
+              <h4 className="font-weight-bold side-link-color">Progress</h4>
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink to="/" className="nav-link d-flex justify-content-center ">
-              <h5>Community</h5>
+          <li className="side-link">
+            <NavLink to="/" className="nav-link mb-5">
+              <h4 className="font-weight-bold side-link-color">Community</h4>
             </NavLink>
           </li>
         </ul>
-        <div>
-          <h1>Daily Vibe</h1>
+        <div className="mt-5 pl-4">
+          <h5 className="font-weight-bold">Daily Vibe</h5>
         </div>
       </nav>
 
