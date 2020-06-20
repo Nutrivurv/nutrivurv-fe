04f085b724d975412040e93c3b307282baaa2ccf
taty2010 <shydalys123@gmail.com>
2020-06-19 23:50:36 -0500

    Initial setup of the gettingStarted component

---
+++ src/components/on-boarding/gettingStarted.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const gettingStarted = () => {
+  return (
+    <>
+      <h2>Let's Get Started!</h2>
+      <p>
+        Staying on target with your weight and dietary goals can be difficult!
+      </p>
+      <p>
+        Nutrivurv makes achieving those goals easier by curating a custom
+        experience tailored to *you*.
+      </p>
+      <p>
+        The information collected in the next few screens will be used to
+        calculate daily caloric and macro-nutrient budget to help you stay on
+        track.
+      </p>
+    </>
+  );
+};
+
+export default gettingStarted;
