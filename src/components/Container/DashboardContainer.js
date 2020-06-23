2df53e178e7972f0d5a094b548f574707586735f
alesslongaretti <alesslongaretti@gmail.com>
2020-06-22 22:52:08 -0400

    added ProtectedRoute to dashboard

--- src/components/Container/DashboardContainer.js
+++ src/components/Container/DashboardContainer.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import SideBar from "../Dashboard/SideBar";
 import Dashboard from "../Dashboard/Dashboard";
+import ProtectedRoute from "../utils/ProtectedRoute";
 
 const DashboardContainer = () => {
   return (
     <div>
       <SideBar />
-      <Route exact path="/dashboard" component={Dashboard} />
+      <ProtectedRoute exact path="/dashboard" component={Dashboard} />
     </div>
   );
 };
