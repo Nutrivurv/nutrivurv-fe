48f54329612e416ef17084db1c35d6a2882eacb7
alesslongaretti <alesslongaretti@gmail.com>
2020-06-22 22:38:42 -0400

    Added both container components to App.js and used Switch

--- src/components/Container/DashboardContainer.js
+++ src/components/Container/DashboardContainer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import SideBar from "../Dashboard/SideBar";
+import Dashboard from "../Dashboard/Dashboard";
 
 const DashboardContainer = () => {
   return (
     <div>
       <SideBar />
-      <Route exact path="/dashboard" />
+      <Route exact path="/dashboard" component={Dashboard} />
     </div>
   );
 };
