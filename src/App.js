dd22459874a6aa79659bfabce18b71060d97af96
Dave Vazquez <dmvaz.li@gmail.com>
2020-07-04 13:30:20 -0400

    moves redux provider to index.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import "bootswatch/dist/flatly/bootstrap.min.css";
 import "./App.scss";
 
 import React from "react";
-import HomeContainer from "./components/Container/HomeContainer";
-import DashboardContainer from "./components/Container/DashboardContainer";
-import { Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
+import { Route, Switch } from "react-router-dom";
 import { store } from "./State/configureStore";
+import DashboardContainer from "./components/Container/DashboardContainer";
+import HomeContainer from "./components/Container/HomeContainer";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <Switch>
-          <Route exact path="/dashboard" component={DashboardContainer} />
-          <Route path="/" component={HomeContainer} />
-        </Switch>
-      </div>
-    </Provider>
+    <div className="App">
+      <Switch>
+        <Route exact path="/dashboard" component={DashboardContainer} />
+        <Route path="/" component={HomeContainer} />
+      </Switch>
+    </div>
   );
 }
 
