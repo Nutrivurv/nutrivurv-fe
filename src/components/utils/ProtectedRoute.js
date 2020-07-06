28303ab9fddbcddb7f1430f6a54d25ed24b7fca2
Dave Vazquez <dmvaz.li@gmail.com>
2020-07-05 23:32:29 -0400

    modifies protected route to only check for jwt
    additional end-point should be created to validate token

--- src/components/utils/ProtectedRoute.js
+++ src/components/utils/ProtectedRoute.js
@@ -1,18 +1,12 @@
 import React from "react";
-import { connect, useSelector } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
-import { RootState } from "../../State/reducers";
-
-var jwt_decode = require("jwt-decode");
 
 const ProtectedRoute = ({ component: Component, ...props }) => {
-  const token = useSelector((state: RootState) => state.auth.token);
-  const token2 = jwt_decode(localStorage.getItem("token"));
   return (
     <Route
       {...props}
       render={() => {
-        if (token2.userId === token.userId) {
+        if (localStorage.getItem("token")) {
           return <Component {...props} />;
         } else {
           return <Redirect to="/signin" {...props} />;
@@ -22,4 +16,4 @@ const ProtectedRoute = ({ component: Component, ...props }) => {
   );
 };
 
-export default connect(null)(ProtectedRoute);
+export default ProtectedRoute;
