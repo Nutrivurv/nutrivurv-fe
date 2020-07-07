bfaccad6d68f2af25e3f20985fadaee64d26e8d7
alesslongaretti <alesslongaretti@gmail.com>
2020-07-07 15:32:02 -0400

    all tests are passing

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -8,6 +8,14 @@ describe("Dashboard", () => {
     });
   });
 
+  it("sidebar food link should work and go to /", () => {
+    cy.get("#food").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/");
+    });
+    cy.go("back");
+  });
+
   it("Toggler should not be visibe", () => {
     cy.get("navbar-toggler").should("not.be.visible");
   });
@@ -15,13 +23,32 @@ describe("Dashboard", () => {
   it("Add to Journal button should work", () => {
     cy.get("#add-journal").click();
   });
+});
 
-  it("sidebar food link should work and go to /", () => {
-    cy.get("#food").click();
+describe("page on small screen size", () => {
+  it("should set viewport to mobile size", () => {
+    cy.viewport(576, 750);
+    cy.get(".navbar-toggler").should("be.visible");
+    cy.get(".navbar-toggler").click();
+    cy.get("#sidebar").find("a").should("be.visible");
+  });
+
+  it("sidebar progress link should work and go to /", () => {
+    cy.get("#progress").click();
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq("/");
     });
-    cy.go("back");
+  });
+});
+
+describe("Dashboard", () => {
+  it("Should successfully complete the signin form and redirect to /dashboard", () => {
+    cy.visit("http://localhost:3000/signin");
+    cy.get("form").within(() => {
+      cy.get('input[name="email"]').type("test@mail.com");
+      cy.get('input[name="password"]').type("test12345");
+      cy.get("[data-cy=submit]").click();
+    });
   });
 
   it("sidebar progress link should work and go to /", () => {
@@ -37,15 +64,5 @@ describe("Dashboard", () => {
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq("/");
     });
-    cy.go("back");
-  });
-});
-
-describe("page on small screen size", () => {
-  it("should set viewport to mobile size", () => {
-    cy.viewport(576, 750);
-    cy.get(".navbar-toggler").should("be.visible");
-    cy.get(".navbar-toggler").click();
-    cy.get("#sidebar").find("a").should("be.visible");
   });
 });
