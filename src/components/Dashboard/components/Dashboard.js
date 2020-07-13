1dd90a531ec46d1a8729561bb339448ec5a6f0df
taty2010 <shydalys123@gmail.com>
2020-07-12 20:09:26 -0500

    checks for token instead is isAuthenticated

--- src/components/Dashboard/components/Dashboard.js
+++ src/components/Dashboard/components/Dashboard.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router";
+import { Redirect, Route, Switch } from "react-router-dom";
 import { logout } from "../../../state/Slices/slices";
-
+import SearchBar from "./SearchBar";
 const Dashboard = () => {
-  const { isAuthenticated } = useSelector((state) => state.auth);
+  const { isAuthenticated, token } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
-  if (!isAuthenticated) return <Redirect to="/" />;
+  if (token === null) return <Redirect to="/" />;
 
   return (
     <div className="d-flex justify-content-end px-5">
-      <button
-        className="btn-primary rounded px-2 py-1 border border-primary"
-        onClick={() => dispatch(logout())}
-      >
-        Log Out
-      </button>
+      <div>
+        <button
+          className="btn-primary rounded px-2 py-1 border border-primary"
+          onClick={() => dispatch(logout())}
+        >
+          Log Out
+        </button>
+      </div>
     </div>
   );
 };
