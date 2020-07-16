d7370c552f8cb1aea4dba7cc635dd22a352417a3
alesslongaretti <alesslongaretti@gmail.com>
2020-07-15 20:54:42 -0400

    added the calculations

--- src/components/Dashboard/components/FoodJournal.js
+++ src/components/Dashboard/components/FoodJournal.js
@@ -1,27 +1,46 @@
 import React, { useState } from "react";
-import { Breakfast, Lunch, Dinner } from "./Data";
+import { data } from "./Data";
 import { ReactComponent as Heart } from "../../../assets/HeartOutline.svg";
 import { ReactComponent as FullHeart } from "../../../assets/FullHeart.svg";
 
 const FoodJournal = () => {
-  const [breakfast, setBreakfast] = useState(Breakfast);
-  const [lunch, setLunch] = useState(Lunch);
-  const [dinner, setDinner] = useState(Dinner);
+  const [breakfast, setBreakfast] = useState(data.Breakfast);
+  const [lunch, setLunch] = useState(data.Lunch);
+  const [dinner, setDinner] = useState(data.Dinner);
   const [favorite, setFavorite] = useState(false);
+
+  const TotalCalories =
+    breakfast.reduce((total, meal) => total + meal.calories, 0) +
+    lunch.reduce((total, meal) => total + meal.calories, 0) +
+    dinner.reduce((total, meal) => total + meal.calories, 0);
 
   const toggleFavorite = () => {
     setFavorite(!favorite);
   };
   return (
-    <div>
-      <table className="table-borderless table w-50 border-none">
+    <div className="w-50">
+      <table className="table-borderless table border-none">
         <thead>
           <tr>
-            <th scope="col">Breakfast</th>
-            <th scope="col">Fat</th>
-            <th scope="col">Protein</th>
-            <th scope="col">Carbs</th>
-            <th scope="col">Calories</th>
+            <th scope="col">
+              <h5 className="font-weight-bold">Breakfast</h5>
+            </th>
+            <th scope="col">
+              {" "}
+              <h5 className="font-weight-bold">Fat</h5>
+            </th>
+            <th scope="col">
+              {" "}
+              <h5 className="font-weight-bold">Protein</h5>{" "}
+            </th>
+            <th scope="col">
+              {" "}
+              <h5 className="font-weight-bold">Carbs</h5>{" "}
+            </th>
+            <th scope="col">
+              {" "}
+              <h5 className="font-weight-bold">Calories</h5>{" "}
+            </th>
           </tr>
         </thead>
         {breakfast.map((data) => {
@@ -29,7 +48,7 @@ const FoodJournal = () => {
             <tbody key={data.id}>
               <tr>
                 <td scope="row">
-                <span className="mr-2">
+                  <span className="mr-2">
                     {favorite ? (
                       <FullHeart onClick={toggleFavorite} />
                     ) : (
@@ -38,16 +57,18 @@ const FoodJournal = () => {
                   </span>
                   {data.food}
                 </td>
-                <td>{data.fat}</td>
-                <td>{data.protein}</td>
-                <td>{data.carbs}</td>
+                <td >{data.fat} g</td>
+                <td>{data.protein} g</td>
+                <td>{data.carbs} g</td>
                 <td>{data.calories}</td>
               </tr>
             </tbody>
           );
         })}
         <thead>
-          <th scope="col">Lunch</th>
+          <th scope="col">
+            <h5 className="font-weight-bold">Lunch</h5>
+          </th>
         </thead>
         {lunch.map((data) => {
           return (
@@ -63,23 +84,25 @@ const FoodJournal = () => {
                   </span>
                   {data.food}
                 </td>
-                <td>{data.fat}</td>
-                <td>{data.protein}</td>
-                <td>{data.carbs}</td>
+                <td>{data.fat} g</td>
+                <td>{data.protein} g</td>
+                <td>{data.carbs} g</td>
                 <td>{data.calories}</td>
               </tr>
             </tbody>
           );
         })}
         <thead>
-          <th scope="col">Dinner</th>
+          <th scope="col">
+            <h5 className="font-weight-bold">Dinner</h5>
+          </th>
         </thead>
         {dinner.map((data) => {
           return (
             <tbody key={data.id}>
               <tr>
                 <td scope="row">
-                <span className="mr-2">
+                  <span className="mr-2">
                     {favorite ? (
                       <FullHeart onClick={toggleFavorite} />
                     ) : (
@@ -88,15 +111,22 @@ const FoodJournal = () => {
                   </span>
                   {data.food}
                 </td>
-                <td>{data.fat}</td>
-                <td>{data.protein}</td>
-                <td>{data.carbs}</td>
+                <td>{data.fat} g</td>
+                <td>{data.protein} g</td>
+                <td>{data.carbs} g</td>
                 <td>{data.calories}</td>
               </tr>
             </tbody>
           );
         })}
+        <th scope="col">
+          <h5 className="font-weight-bold">Snack</h5>
+        </th>
       </table>
+      <div className="d-flex justify-content-between">
+        <p>Total Water:</p>
+        <p>Total Calories:{TotalCalories}</p>
+      </div>
     </div>
   );
 };
--- src/components/Dashboard/components/JournalContainer.js
+++ src/components/Dashboard/components/JournalContainer.js
@@ -5,12 +5,12 @@ import { Tabs, TabLink, TabContent } from "react-tabs-redux";
 
 const JournalContainer = () => {
   return (
-    <Tabs>
-      <TabLink to="tab1" className="font-weight-bold">
-        Food Journal
+    <Tabs activeLinkStyle={{}}>
+      <TabLink to="tab1" className="font-weight-bold border-0 bg-white">
+        <h4 className="font-weight-bolder">Food Journal</h4>
       </TabLink>
-      <TabLink to="tab2" className="font-weight-bold">
-        Favorites
+      <TabLink to="tab2" className="font-weight-bold border-0 bg-white">
+        <h4 className="font-weight-bolder">Favorites</h4>
       </TabLink>
 
       <TabContent for="tab1">
