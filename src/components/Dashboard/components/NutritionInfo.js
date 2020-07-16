4b553440b3ac89b65144ad733745196a8ea3529a
Christine Smith <christinecsmith1@gmail.com>
2020-07-15 20:01:14 -0600

    fixed measures dropdown bug

--- src/components/Dashboard/components/NutritionInfo.js
+++ src/components/Dashboard/components/NutritionInfo.js
@@ -4,12 +4,20 @@ import DropDown from "./DropDown";
 
 const NutritionInfo = (props) => {
   const { items } = useSelector((state) => state.auth);
-  console.log("items", items);
+  var selectedItemInList = items.filter(function (item) {
+    return item.food.foodId === props.selectedFoodId;
+  });
+  var selectedItemObject = selectedItemInList[0];
+
   return (
     <div className="d-block">
       <h5>You clicked an item!</h5>
       <p>Selected Item: {JSON.stringify(props.selectedItem)}</p>
-      <DropDown items={items} selectedItem={props.selectedItem}/>
+      <DropDown
+        items={items}
+        selectedItem={props.selectedItem}
+        selectedItemObject={selectedItemObject}
+      />
       <img src={props.selectedItem.image} />
       <h2>{props.selectedItem.label}</h2>
     </div>
