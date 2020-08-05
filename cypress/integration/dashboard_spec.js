d9b8cbac509ea6e4339fac40d890f796174e6da2
taty2010 <shydalys123@gmail.com>
2020-08-04 20:38:35 -0500

    updates

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -1,99 +1,50 @@
 describe("Dashboard", () => {
   it("Should successfully complete the signin form and redirect to /dashboard", () => {
-    cy.visit("http://localhost:3000/signin");
+    cy.visit("/signin");
     cy.typeLogin({ email: "test@mail.com", password: "test12345" });
     cy.get("[data-cy=submit]").click();
-  });
-
-  it("sidebar food link should work and go to /", () => {
-    cy.get("#food").click();
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq("/");
-    });
-    cy.go("back");
   });
 
   it("Toggler should not be visibe", () => {
     cy.get("navbar-toggler").should("not.be.visible");
   });
-});
-
-describe("page on small screen size", () => {
+  it("Sidebar Budget should populate data", () => {
+    cy.get(".data").should("be.visible");
+    cy.get("#calories").contains("2214 kcal");
+    cy.get("#fats").contains("62 g");
+    cy.get("#carbs").contains("123 g");
+    cy.get("#protein").contains("62 g");
+  });
   it("should set viewport to mobile size", () => {
     cy.viewport(576, 750);
     cy.get(".navbar-toggler").should("be.visible");
     cy.get(".navbar-toggler").click();
     cy.get("#sidebar").find("a").should("be.visible");
   });
-
-  it("sidebar progress link should work and go to /", () => {
-    cy.get("#progress").click();
+  it("Settings button should have dropdown", () => {
+    cy.get("#dropdownMenuButton").click();
+    cy.get("#profile").should("be.visible");
+    cy.get("#logout").click();
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq("/");
     });
   });
 });
 
-describe("Searchbar", () => {
-  it("Add to Journal button should work", () => {
-    cy.visit("http://localhost:3000/signin");
-    cy.typeLogin({ email: "test@mail.com", password: "test12345" });
-    cy.get("[data-cy=submit]").click();
-    cy.get("#add-journal").click();
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq("/dashboard/food-search");
-    });
-  });
-  it("Searchbar should function", () => {
-    cy.get("#inputSearch").type("Bacon");
-    cy.get("[data-cy=submit]").click();
-  });
-  it("Loader should popup when searching", () => {
-    cy.get(".loader").should("be.visible");
-  });
-  it("Search Results List should appear", () => {
-    cy.get("#searchList").should("be.visible");
-    cy.get("#searchList").contains("Bacon");
-  });
-  it("Should be able to Click on item in list", () => {
-    cy.get("#searchItem").should("be.visible");
-    cy.get("#searchItem").click();
-  });
-  it("Nutrition Card should appear after item click", () => {
-    cy.get(".nutrition").should("be.visible");
-  });
-});
-
-describe("Dashboard", () => {
-  it("Should successfully complete the signin form and redirect to /dashboard", () => {
-    cy.visit("http://localhost:3000/signin");
+describe("Dashboard logo and user profile link", () => {
+  before("Login to the application", function () {
+    cy.visit("/signin");
     cy.typeLogin({ email: "test@mail.com", password: "test12345" });
     cy.get("[data-cy=submit]").click();
   });
-
-  it("sidebar progress link should work and go to /", () => {
-    cy.get("#progress").click();
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq("/");
-    });
-    cy.go("back");
-  });
-
-  it("sidebar community link should work and go to /", () => {
-    cy.get("#progress").click();
+  it("Logo should be visible and link to Home", () => {
+    cy.get(".logoDash").should("be.visible");
+    cy.get(".logoDash").click();
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq("/");
     });
   });
-
-  it("Sidebar Budget should populate data", () => {
-    cy.visit("/dashboard");
-    cy.typeLogin({ email: "test@mail.com", password: "test12345" });
-    cy.get("[data-cy=submit]").click();
-    cy.get(".data").should("be.visible");
-    cy.get("#calories").contains("2214 kcal");
-    cy.get("#fats").contains("62 g");
-    cy.get("#carbs").contains("123 g");
-    cy.get("#protein").contains("62 g");
+  it("Profile link should be visible", () => {
+    cy.get("#profileLink").should("be.visible");
   });
 });
