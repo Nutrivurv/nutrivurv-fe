243339daafdc0f89bc6949ce8a9dc7f958918378
alesslongaretti <alesslongaretti@gmail.com>
2020-08-05 15:21:13 -0400

    mapped the dinner meals - they are showing up on the food journal

--- src/components/Dashboard/components/FoodJournal.js
+++ src/components/Dashboard/components/FoodJournal.js
@@ -114,6 +114,29 @@ const FoodJournal = () => {
             <h5 className="font-weight-bold">Dinner</h5>
           </th>
         </thead>
+        {entries.meals.dinner &&
+          entries.meals.dinner.map((data) => {
+            return (
+              <tbody key={data.id}>
+                <tr>
+                  <td scope="row">
+                    <span className="mr-2">
+                      {favorite ? (
+                        <FullHeart onClick={toggleFavorite} />
+                      ) : (
+                        <Heart onClick={toggleFavorite} />
+                      )}
+                    </span>
+                    {data.food_name}
+                  </td>
+                  <td>{data.fat_g} g</td>
+                  <td>{data.protein_g} g</td>
+                  <td>{data.carbs_g} g</td>
+                  <td>{data.calories_kcal}</td>
+                </tr>
+              </tbody>
+            );
+          })}
         <th scope="col">
           <h5 className="font-weight-bold">Snack</h5>
         </th>
