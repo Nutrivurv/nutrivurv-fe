c651506aa200ee3e49800728ac80e14a9722532e
alesslongaretti <alesslongaretti@gmail.com>
2020-08-06 22:49:28 -0400

    added a snack component

--- src/components/Dashboard/components/FoodJournal/Components/Snack.js
+++ src/components/Dashboard/components/FoodJournal/Components/Snack.js
@@ -5,7 +5,36 @@ import { ReactComponent as Heart } from "../../../../../assets/HeartOutline.svg"
 const Snack = (props) => {
   return (
     <div>
-      </div>
+      {props.entries.meals.snack &&
+        props.entries.meals.snack.map((data) => {
+          return (
+            <tbody
+              onClick={(e) => {
+                e.preventDefault();
+                props.handleItemClick(data);
+              }}
+              key={data.id}
+            >
+              <tr>
+                <td scope="row">
+                  <span className="mr-2">
+                    {props.favorite ? (
+                      <FullHeart onClick={props.toggleFavorite} />
+                    ) : (
+                      <Heart onClick={props.toggleFavorite} />
+                    )}
+                  </span>
+                  {data.food_name}
+                </td>
+                <td>{data.fat_g} g</td>
+                <td>{data.protein_g} g</td>
+                <td>{data.carbs_g} g</td>
+                <td>{data.calories_kcal}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+    </div>
   );
 };
 
