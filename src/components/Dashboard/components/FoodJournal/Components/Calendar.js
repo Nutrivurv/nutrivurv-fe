bda8f22148bf19a5b44dcddce92bd0910d46a9bb
alesslongaretti <alesslongaretti@gmail.com>
2020-08-07 00:24:20 -0400

    functionality for back calendar arrow

--- src/components/Dashboard/components/FoodJournal/Components/Calendar.js
+++ src/components/Dashboard/components/FoodJournal/Components/Calendar.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
 
-const Calendar = () => {
-    return (
-        <div>
-            <button onClick={} value={startDate} type="hover"></button>
-        <input
-          className=""
-          type="date"
-          value={moment(startDate).format("YYYY-MM-DD")}
-          onChange={(e) => setStartDate(e.target.value)}
-        />
-        <button onClick={} type="hover"></button>
+const Calendar = (props) => {
+  const [date, setDate] = useState(1);
 
-        </div>
-    )
+  let backDateArray = [];
 
+  const backDate = () => {
+    let a = moment();
+    let b = moment("2010-08-07");
+    for (let m = a; m.isAfter(b); m.subtract(1, "day")) {
+      backDateArray.push(m.format("YYYY-MM-DD"));
+    }
+
+    for (let i = 0; i < backDateArray.length; i++) {
+      if (date === i) {
+        console.log(date);
+        setDate(date + 1);
+        props.setStartDate(backDateArray[i]);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={backDate} value={props.startDate} type="hover"></button>
+      <input
+        className=""
+        type="date"
+        value={moment(props.startDate).format("YYYY-MM-DD")}
+        onChange={(e) => props.setStartDate(e.target.value)}
+      />
+      {/* <button onClick={} type="hover"></button> */}
+    </div>
+  );
 };
 
 export default Calendar;
