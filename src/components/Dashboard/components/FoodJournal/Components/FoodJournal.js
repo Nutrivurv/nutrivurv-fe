e2727a9bdfbab4f17d69632bdf147a1c284dd586
alesslongaretti <alesslongaretti@gmail.com>
2020-08-06 22:49:14 -0400

    added a dinner component

--- src/components/Dashboard/components/FoodJournal/Components/FoodJournal.js
+++ src/components/Dashboard/components/FoodJournal/Components/FoodJournal.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import { useDispatch, useSelector } from "react-redux";
-import { ReactComponent as FullHeart } from "../../../../../assets/FullHeart.svg";
-import { ReactComponent as Heart } from "../../../../../assets/HeartOutline.svg";
 import { getFoodLogEntries } from "../../../../../state/slices/userinfo";
 import JournalNutritionInfo from "../../JournalNutritionInfo";
 import { getNutrients } from "../../../../../state/slices/EdamamSlice";
 import Breakfast from "./Breakfast";
 import Lunch from "./Lunch";
+import Dinner from "./Dinner";
+import Snack from "./Snack";
 import moment from "moment";
 
 const FoodJournal = () => {
@@ -89,81 +89,34 @@ const FoodJournal = () => {
             <h5 className="font-weight-bold">Lunch</h5>
           </th>
         </thead>
-        <Lunch entries={entries}
+        <Lunch
+          entries={entries}
           handleItemClick={handleItemClick}
           favorite={favorite}
-          toggleFavorite={toggleFavorite}/>
+          toggleFavorite={toggleFavorite}
+        />
         <thead>
           <th scope="col">
             <h5 className="font-weight-bold">Dinner</h5>
           </th>
         </thead>
-        {entries.meals.dinner &&
-          entries.meals.dinner.map((data) => {
-            return (
-              <tbody
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleItemClick(data);
-                }}
-                key={data.id}
-              >
-                <tr>
-                  <td scope="row">
-                    <span className="mr-2">
-                      {favorite ? (
-                        <FullHeart onClick={toggleFavorite} />
-                      ) : (
-                        <Heart onClick={toggleFavorite} />
-                      )}
-                    </span>
-                    {data.food_name}
-                  </td>
-                  <td>{data.fat_g} g</td>
-                  <td>{data.protein_g} g</td>
-                  <td>{data.carbs_g} g</td>
-                  <td>{data.calories_kcal}</td>
-                </tr>
-              </tbody>
-            );
-          })}
+        <Dinner
+          entries={entries}
+          handleItemClick={handleItemClick}
+          favorite={favorite}
+          toggleFavorite={toggleFavorite}
+        />
         <thead>
           <th scope="col">
             <h5 className="font-weight-bold">Snack</h5>
           </th>
         </thead>
-        {entries.meals.snack &&
-          entries.meals.snack.map((data) => {
-            return (
-              <tbody
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleItemClick(data);
-                }}
-                key={data.id}
-              >
-                <tr>
-                  <td scope="row">
-                    <span className="mr-2">
-                      {favorite ? (
-                        <FullHeart onClick={toggleFavorite} />
-                      ) : (
-                        <Heart onClick={toggleFavorite} />
-                      )}
-                    </span>
-                    {data.food_name}
-                  </td>
-                  <td>{data.fat_g} g</td>
-                  <td>{data.protein_g} g</td>
-                  <td>{data.carbs_g} g</td>
-                  <td>{data.calories_kcal}</td>
-                </tr>
-              </tbody>
-            );
-          })}
-        <th scope="col">
-          <h5 className="font-weight-bold">Snack</h5>
-        </th>
+        <Snack
+          entries={entries}
+          handleItemClick={handleItemClick}
+          favorite={favorite}
+          toggleFavorite={toggleFavorite}
+        />
       </table>
       <div className="d-flex justify-content-between">
         <p>Total Water:</p>
