1b42b9e66939906a7298ff6971f7d3141a8ca62d
alesslongaretti <alesslongaretti@gmail.com>
2020-08-07 00:28:41 -0400

    functionality for fwd calendar arrow

--- src/components/Dashboard/components/FoodJournal/Components/Calendar.js
+++ src/components/Dashboard/components/FoodJournal/Components/Calendar.js
@@ -3,8 +3,10 @@ import moment from "moment";
 
 const Calendar = (props) => {
   const [date, setDate] = useState(1);
+  const [fwdDate, setFwdDate] = useState(0);
 
   let backDateArray = [];
+  let forwardDateArray = [];
 
   const backDate = () => {
     let a = moment();
@@ -22,6 +24,23 @@ const Calendar = (props) => {
     }
   };
 
+  const forwardDate = () => {
+    let a = moment();
+    let b = moment(props.startDate);
+    for (let m = b; m.isBefore(a); m.add(1, "day")) {
+      forwardDateArray.push(m.format("YYYY-MM-DD"));
+    }
+    for (let i = 0; i < forwardDateArray.length; i++) {
+      if (date > i) {
+        setDate(date - 1);
+        props.setStartDate(forwardDateArray[1]);
+      }
+    }
+    if (fwdDate === forwardDateArray.length) {
+      setFwdDate(0);
+    }
+  };
+
   return (
     <div>
       <button onClick={backDate} value={props.startDate} type="hover"></button>
@@ -31,7 +50,7 @@ const Calendar = (props) => {
         value={moment(props.startDate).format("YYYY-MM-DD")}
         onChange={(e) => props.setStartDate(e.target.value)}
       />
-      {/* <button onClick={} type="hover"></button> */}
+      <button onClick={forwardDate} type="hover"></button>
     </div>
   );
 };
