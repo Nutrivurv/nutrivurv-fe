c3a18bfd347ce1dbcf65d68f2fc4f78483129557
alesslongaretti <alesslongaretti@gmail.com>
2020-08-11 19:59:00 -0400

    added confirmation modal to the rest of the components

--- src/components/Dashboard/components/FoodJournal/Components/Snack.js
+++ src/components/Dashboard/components/FoodJournal/Components/Snack.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as FullHeart } from "../../../../../assets/FullHeart.svg";
 import { ReactComponent as Heart } from "../../../../../assets/HeartOutline.svg";
 import { deleteFoodLogEntries } from "../../../../../state/slices/userinfo";
 import { useDispatch } from "react-redux";
 import { ReactComponent as TrashCan } from "../../../../../assets/Trash-can.svg";
+import { Button, Modal } from "react-bootstrap";
 
 const Snack = (props) => {
   const dispatch = useDispatch();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <tbody>
       {props.entries.meals.snack &&
@@ -33,13 +40,29 @@ const Snack = (props) => {
               <td>{data.protein_g} g</td>
               <td>{data.carbs_g} g</td>
               <td>{data.calories_kcal}</td>
-              <TrashCan
-                className="mt-2"
-                onClick={(e) => {
-                  e.preventDefault();
-                  dispatch(deleteFoodLogEntries(data.id));
-                }}
-              />
+              <TrashCan className="mt-2" onClick={handleShow} />
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>
+                    Are you sure you want to delete this item?
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Cancel
+                  </Button>
+                  <Button
+                    variant="primary"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      dispatch(deleteFoodLogEntries(data.id));
+                      handleClose();
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </Modal.Footer>
+              </Modal>
             </tr>
           );
         })}{" "}
